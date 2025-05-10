5c0f5aa0704a5cd1b9d7bf18b48ac7eba3caad88
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2025-05-09 21:50:59 -0700

    Added Movie table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,9 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Movie: a
     .model({
-      content: a.string(),
+      title: a.string(),
     })
     .authorization((allow) => [allow.owner()]),
 });
@@ -32,7 +32,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
