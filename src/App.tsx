23250b537caf362747b03d595cec361c23050ddd
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2025-05-09 22:08:13 -0700

    Changed front end

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,31 @@ import { useAuthenticator } from '@aws-amplify/ui-react';
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [movies, setTodos] = useState<Array<Schema["Movie"]["type"]>>([]);
   const { user, signOut } = useAuthenticator();
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    client.models.Movie.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Movie.create({ title: window.prompt("Enter a moive title") });
   }
 
   return (
     <main>
       <button onClick={signOut}>Sign out</button>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s moive watch list</h1>
+      <h1>My movie watch list</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {movies.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
-      
+
     </main>
   );
 }
