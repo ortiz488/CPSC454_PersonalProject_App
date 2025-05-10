0f4bd8470c1483728ddd5ef56055ac5d66cbc166
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2025-05-09 22:56:52 -0700

    Changed UI 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import Movie_background from "./assets/Movie_background.jpg";
 
 const client = generateClient<Schema>();
 
@@ -20,19 +21,28 @@ function App() {
   }
 
   return (
-    <main>
-      <div className="app-container">
-        <button onClick={signOut}>Sign Out</button>
-      </div>
-
-      <h1>{user?.signInDetails?.loginId}'s moive watch list</h1>
+    <main
+      style={{
+        backgroundImage: `url(${Movie_background})`,   // <-- your page background
+        minHeight: "100vh",           // fill full viewport
+        padding: "2rem",
+        textAlign: "center",
+        position: "relative",         // for any absolutely-positioned children
+      }}
+    >
+      <h1>Current signed user = {user?.signInDetails?.loginId}</h1>
       <h1>My movie watch list</h1>
-      <button onClick={createTodo}>+ new</button>
+
       <ul>
         {movies.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
+
+      <div className="app-container">
+        <button onClick={signOut}>Sign Out</button>
+        <button onClick={createTodo}>Add Moive</button>
+      </div>
 
     </main>
   );
